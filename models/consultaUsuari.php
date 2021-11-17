bac6598862f9d03252667fd10576090447aee763
Lola Barberán Baeta <Lola.Barberan@e-msoffice.uab.cat>
2021-11-17 01:25:40 +0100

    modificació

--- models/consultaUsuari.php
+++ models/consultaUsuari.php
@@ -2,7 +2,7 @@
 <?php
 function getUsuaris($connexio){
     try {
-        $consulta_usuaris = $connexio->prepare("SELECT id, correu, contrasenya, nom, població, codi_postal FROM usuari");
+        $consulta_usuaris = $connexio->prepare("SELECT id, nom, correu, contrasenya, adreca, poblacio, codi_postal FROM usuari");
         $consulta_usuaris->execute();
         $usuaris = $consulta_usuaris->fetchAll(PDO::FETCH_ASSOC);
     } catch(PDOException $e) {
