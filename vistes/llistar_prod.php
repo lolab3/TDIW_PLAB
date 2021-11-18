9c48334ee03515cb0b060e4e12db8e24af37466b
arnauraich10 <arnauista@hotmail.com>
2021-11-18 09:45:51 +0100

    Update

---
+++ vistes/llistar_prod.php
@@ -0,0 +1,7 @@
+<div class="producte">
+    <img src="/../imgs/producte<?php echo $producte['id'] ?>"></img>
+    <h1><?php echo $producte['nom'] ?></h1>
+    <p><?php echo $producte['descripció'] ?></p>
+    <h2><?php echo $producte['preu_actual'] ?></h2>
+    <button class="boto-afegir"> Afegir </button>
+</div>
