1779370bbb0d494b6419c5fb2c981fe2af026d84
arnauraich10 <arnauista@hotmail.com>
2021-12-15 10:08:31 +0100

    Pagina carro de la compra

--- vistes/llistar_carro.php
+++ vistes/llistar_carro.php
@@ -1,9 +1,24 @@
-<html>
-<div class="producte_carro">
-    <img src="/TDIW_PLAB/imgs/producte1.webp" style="grid-area: imatge">
-    <h3 style="grid-area: titol">Nom producte</h3>
-    <p style="grid-area: descripcio">Descripcio producte</p>
-    <button style="grid-area: eliminar">Eliminar producte</button>
-    <button style="grid-area: afegir">Afegir mes quantitat</button>
+<div class="carro_total">
+    <h2>Carro compra</h2>
+    <ul class="carro_table">
+        <li class="header_table">
+            <div class="data">Producte</div>
+            <div class="data">Nom</div>
+            <div class="data">Preu</div>
+            <div class="data">Modificar quantitat</div>
+        </li>
+        <li class="fila_table">
+            <div class="data" ><img src="/TDIW_PLAB/imgs/producte1.webp"></div>
+            <div class="data" >jaqueta</div>
+            <div class="data" >$350</div>
+            <div class="data" ><button>-1</button><button>+1</button></div>
+        </li>
+        <li class="fila_table">
+            <div class="data" ><img src="/TDIW_PLAB/imgs/producte2.webp"></div>
+            <div class="data" >Pantalons</div>
+            <div class="data" >$220</div>
+            <div class="data" ><button>-1</button><button>+1</button></div>
+        </li>
+
+    </ul>
 </div>
-</html>
