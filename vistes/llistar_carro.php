1d51c2718c5d3da5b78ab3c4bfb95db66af4fed0
arnauraich10 <arnauista@hotmail.com>
2021-12-15 12:11:13 +0100

    Pagina carro de la compra update

--- vistes/llistar_carro.php
+++ vistes/llistar_carro.php
@@ -1,24 +1,24 @@
 <div class="carro_total">
     <h2>Carro compra</h2>
-    <ul class="carro_table">
-        <li class="header_table">
-            <div class="data">Producte</div>
-            <div class="data">Nom</div>
-            <div class="data">Preu</div>
-            <div class="data">Modificar quantitat</div>
-        </li>
-        <li class="fila_table">
-            <div class="data" ><img src="/TDIW_PLAB/imgs/producte1.webp"></div>
-            <div class="data" >jaqueta</div>
-            <div class="data" >$350</div>
-            <div class="data" ><button>-1</button><button>+1</button></div>
-        </li>
-        <li class="fila_table">
-            <div class="data" ><img src="/TDIW_PLAB/imgs/producte2.webp"></div>
-            <div class="data" >Pantalons</div>
-            <div class="data" >$220</div>
-            <div class="data" ><button>-1</button><button>+1</button></div>
-        </li>
+    <table class="carro_table">
+        <tr class="header_table">
+            <th class="data">Producte</th>
+            <th class="data">Nom</th>
+            <th class="data">Preu</th>
+            <th class="data">Quantitat</th>
+        </tr>
+        <tr class="fila_table">
+            <td class="data" >Jaqueta</td>
+            <td class="data" >Jaqueta</td>
+            <td class="data" >$350</td>
+            <td class="data" ><button>-1</button><div class="box_quantitat">2</div><button>+1</button></td>
+        </tr>
+        <tr class="fila_table">
+            <td class="data" ><img src="/TDIW_PLAB/imgs/producte2.webp"></td>
+            <td class="data" >Pantalons</td>
+            <td class="data" >$220</td>
+            <td class="data" ><button>-1</button><button>+1</button></td>
+        </tr>
 
-    </ul>
+    </table>
 </div>
