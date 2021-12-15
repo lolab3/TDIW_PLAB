1dde797905990031affdbf741eeeda7764293e85
Lola Barberán Baeta <Lola.Barberan@e-msoffice.uab.cat>
2021-12-15 01:09:31 +0100

    modificacions seguretat registre

--- vistes/llistar_registre.php
+++ vistes/llistar_registre.php
@@ -10,7 +10,7 @@
         <h1>Crea el teu compte ara</h1>
         <div class="formulari">
             <div class="form-item" style="grid-area: nom;">
-                <p>Nom: </p> <input type="text" name="nom" placeholder="Nom complet">
+                <p>Nom: </p> <input type="text" name="nom" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)" placeholder="Nom complet">
             </div>
             <div class="form-item" style="grid-area: correu;">
                 <p>Correu electronic: </p> <input type="email" name="correu" placeholder="email">
@@ -22,7 +22,7 @@
                 <p>Adreça: </p> <input type="text" name="adreca" required maxlength="30" placeholder="Adreça">
             </div>
             <div class="form-item" style="grid-area: poblacio">
-                <p>Població:</p> <input type="text" name="poblacio" required maxlength="30" placeholder="Població">
+                <p>Població:</p> <input type="text" name="poblacio" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)" required maxlength="30" placeholder="Població">
             </div>
             <div class="form-item" style="grid-area: codi_postal;">
                 <p>Codi postal:</p> <input type="number" name="codi_postal" size="5" min="1" max="99999" placeholder="Codi Postal">
