8c12a45efebbed983c4f8d20b07364274c7bf365
Lola Barberán Baeta <Lola.Barberan@e-msoffice.uab.cat>
2021-12-16 13:43:25 +0100

    modificacions menu superior

--- controladors/menu_superior.php
+++ controladors/menu_superior.php
@@ -7,6 +7,10 @@
     $connexio = connectaBD();
     $categories = getCategories($connexio); // Aquesta crida és al model
 
+    foreach($categories as $num=>$fila){
+        $categories[$num]['tipus'] = htmlentities($fila['tipus'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    }
+
     include_once __DIR__.'/../vistes/llistar_menu.php';
 
 ?>
