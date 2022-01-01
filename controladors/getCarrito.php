00bb2584f81287a22c9e2a3521cebb5f92fa2eb4
Lola <lolabarberan1@gmail.com>
2022-01-01 13:38:53 +0100

    carrito

--- controladors/getCarrito.php
+++ controladors/getCarrito.php
@@ -3,20 +3,12 @@
     require_once __DIR__.'/../models/consultaProds.php';
 
     $llista_ids = array_values(array_unique($_SESSION['cistella']));
+    $quantitat_prod_indiv = array_count_values($_SESSION['cistella']);
     $quantitat_prods = $_SESSION['total_productes'];
     $preu_total = $_SESSION['preu_total'];
 
     $connexio = connectaBD();
     $resultat = getProd($connexio, $llista_ids);
-
-    if($resultat[0]) {
-        #echo "Correcte";
-        #include __DIR__ .'/iniciar_sessio.php';
-    } else{
-        #echo $errorLogin;
-        #$errorLogin = $resultat[1];
-        #include __DIR__ . '/../vistes/comprovaUsuariinc.php';
-    }
 
     include __DIR__ . '/../vistes/llistar_carro.php';
 ?>
--- vistes/llistar_carro.php
+++ vistes/llistar_carro.php
@@ -3,22 +3,27 @@
     <h2>Carro compra</h2>
     <table class="carro_table">
         <tr class="header_table">
-            <th class="data">Imatge</th>
             <th class="data">Nom</th>
             <th class="data">Preu unitari</th>
             <th class="data">Quantitat</th>
         </tr>
+        <?php $n=0; ?>
         <?php if(isset($_SESSION['ID'])): ?>
-            <?php print_r($_SESSION); ?>
-            <?php foreach ($_SESSION as $a => $i): ?>
+            <?php foreach ($resultat as $i): ?>
                 <tr class="fila_table">
-                <?php foreach($i as $b => $o): ?>
-                    <td class="data"><?php echo $o; ?></td>
-                <?php endforeach; ?>
+                    <?php foreach($i as $b): ?>
+                        <td class="data"><?php echo $b; ?></td>
+                    <?php endforeach; ?>
+                    <td class="data"><?php echo $quantitat_prod_indiv[$llista_ids[$n]]; ?></td>
                 </tr>
+            <?php $n++; ?>
             <?php endforeach; ?>
         <?php endif; ?>
     </table>
+
+    <?php echo "Total de productes: $quantitat_prods"?>
+    <?php echo "Preu total: $preu_total"?>
+
 </div>
 
 
