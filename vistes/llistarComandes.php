80105cbc713c4aaf93aa4503957aee5c4e485f19
Lola <lolabarberan1@gmail.com>
2022-01-02 13:59:09 +0100

    retocs

--- vistes/llistarComandes.php
+++ vistes/llistarComandes.php
@@ -8,7 +8,7 @@
         <?php foreach ($prodsComanda[$n] as $prodUnic):?>
             <h3><?php echo "Article nº $a:";?></h3>
             <h4><?php echo "Nom del producte: {$prodUnic['nom_producte']}";?></h4>
-            <h4><?php echo "Preu: {$prodUnic['preu_historic']}";?></h4>
+            <h4><?php echo "Preu: {$prodUnic['preu_historic']}€";?></h4>
             <h4><?php echo nl2br("Unitats: {$prodUnic['unitat_productes']}\n");?></h4>
             <?php $a = $a+1; ?>
             <?php echo nl2br("\n");?>
@@ -16,7 +16,7 @@
         <?php $a = 1; ?>
         <?php $n = $n+1; ?>
         <h3><?php echo "Total de productes: {$prod['total_productes']}";?></h3>
-        <h3><?php echo "Cost total: {$prod['preu_total']}";?></h3>
+        <h3><?php echo "Cost total: {$prod['preu_total']}€";?></h3>
         <?php echo nl2br("\n\n\n");?>
     <?php endforeach; ?>
 </div>
